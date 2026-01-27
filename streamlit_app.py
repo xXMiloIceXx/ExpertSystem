75329de5e8301d2d08723b90578430f0e4a7137a
Element944 <jiangching@gmail.com>
2026-01-27 12:45:31 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,66 +43,15 @@ st.info(
 # ======================================
 st.subheader("Select Observed Symptoms")
 
-# Question 1
-st.markdown("**1. Does the PC power on?**")
-power = st.radio(
-    label="",
-    options=["Yes", "No"],
-    index=None,
-    key="power",
-    horizontal=True
-)
+col1, col2 = st.columns(2)
 
-# Question 2
-st.markdown("**2. Are there diagnostic beeps during startup?**")
-beeps = st.radio(
-    label="",
-    options=["Yes", "No"],
-    index=None,
-    key="beeps",
-    horizontal=True
-)
+with col1:
+    power = st.radio("Does the PC power on?", ["Yes", "No"], index=None)
+    beeps = st.radio("Are there diagnostic beeps?", ["Yes", "No"], index=None)
 
-# Question 3
-st.markdown("**3. Is there any display on the screen?**")
-screen = st.radio(
-    label="",
-    options=["Visible", "Black/Blank"],
-    index=None,
-    key="screen",
-    horizontal=True
-)
-
-# Question 4
-st.markdown("**4. Does the PC shut down unexpectedly after powering on?**")
-shutdown = st.checkbox(
-    "Yes",
-    key="shutdown"
-)
-
-# Question 5
-st.markdown("**5. Do you see a ‘No Bootable Device’ error message?**")
-boot_error = st.checkbox(
-    "Yes",
-    key="boot_error"
-)
-
-# Question 6
-st.markdown("**6. Does the system time or date reset frequently?**")
-time_reset = st.checkbox(
-    "Yes",
-    key="time_reset"
-)
-
-# Question 7
-st.markdown("**7. Are there system test failures or abnormal startup behaviour?**")
-test = st.radio(
-    label="",
-    options=["Yes", "No"],
-    index=None,
-    key="test",
-    horizontal=True
-)
+with col2:
+    screen = st.radio("Is there any display on the screen?", ["Visible", "Black/Blank"], index=None)
+    shutdown = st.checkbox("PC shuts down unexpectedly?")
 
 # ======================================
 # Diagnosis Execution
@@ -174,10 +123,7 @@ if st.button("Start Diagnosis", use_container_width=True):
 # System Reset
 # ======================================
 if st.button("Reset System"):
-    for key in [
-        "power", "beeps", "screen", "shutdown",
-        "boot_error", "time_reset", "test"
-    ]:
+    for key in ["power", "beeps", "screen", "shutdown"]:
         if key in st.session_state:
             del st.session_state[key]
     st.rerun()
