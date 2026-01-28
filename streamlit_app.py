84ea3692622e84ae989f00e568e3e36382fad2f9
Element944 <jiangching@gmail.com>
2026-01-29 01:38:01 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -341,7 +341,7 @@ elif st.session_state.step == 10:
             st.subheader("📚 Feedback")
             
             # You can change this URL to any link you want
-            external_url = "https://www.google.com/"
+            external_url = "https://docs.google.com/forms/d/e/1FAIpQLScGm5kkIxK88AZM_ElaVZDwIUqQgCG_kP7ficPKa9H3T6QAgQ/viewform?usp=publish-editor"
             
             # Alternative approach using link
             st.markdown(f"[🌐 **Google Form Submission**]({external_url})", unsafe_allow_html=True)
